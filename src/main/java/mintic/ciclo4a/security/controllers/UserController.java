5335403fc9e56a48c2497ae9656720c2682f703b
Samuel Pérez <samusinho79@gmail.com>
2022-10-26 20:23:03 -0500

    metodo DELETE y UPDATE para usuarios

--- src/main/java/mintic/ciclo4a/security/controllers/UserController.java
+++ src/main/java/mintic/ciclo4a/security/controllers/UserController.java
@@ -3,7 +3,10 @@ package mintic.ciclo4a.security.controllers;
 import lombok.AllArgsConstructor;
 import mintic.ciclo4a.security.models.User;
 import mintic.ciclo4a.security.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,10 +27,34 @@ public class UserController {
     // GetMapping -> Método GET
     // "" -> "/users"
     @GetMapping("")
-    List<User> getUsers(@RequestParam("nick") Optional<String> nick) {
+    ResponseEntity<List<User>> getUsers(@RequestParam("nick") Optional<String> nick) {
         if (nick.isPresent()) {
-            return userRepository.findByRegexpNick(nick.get());
+            return ResponseEntity.status(200).body(userRepository.findByRegexpNick(nick.get()));
+            // return userRepository.findByRegexpNick(nick.get());
         }
-        return userRepository.findAll();
+
+        return ResponseEntity.status(200).body(userRepository.findAll());
+    }
+
+    // @PathVariable("userId") -> /users/{userId}
+    @GetMapping("{userId}")
+    User getUserById(@PathVariable("userId") String userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "El usuario no existe"));
+    }
+
+    @DeleteMapping("{userId}")
+    ResponseEntity<String> deleteUser(@PathVariable("userId") String userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "El usuario no existe"));
+        userRepository.delete(user);
+        return ResponseEntity.ok().body("El usuario " + user.getNick() + " Fue eliminado");
+    }
+
+    @PutMapping("{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody User userinfo) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "El usuario no existe"));
+        user.setNick(userinfo.getNick());
+        user.setEmail(userinfo.getEmail());
+        return userRepository.save(user);
     }
 }
